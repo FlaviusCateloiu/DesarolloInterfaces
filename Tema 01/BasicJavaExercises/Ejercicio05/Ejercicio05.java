b198f706ec5ccee328e321dcbc9eda7400a0cbf6
flaars <fcateloiu08@gmail.com>
2022-09-30 19:02:15 +0200

    Corregido el ejercicio05

--- Tema 01/BasicJavaExercises/Ejercicio05/Ejercicio05.java
+++ Tema 01/BasicJavaExercises/Ejercicio05/Ejercicio05.java
@@ -5,15 +5,12 @@ import java.io.Reader;
 public class Ejercicio05 {
     public static void main(String[] args) throws IOException {
         Reader reader = null;
-        try {
-            reader = new FileReader("Tema 01/BasicJavaExercises/Ejercicio05/file.txt");
-            int current = reader.read();
-            while (current != -1) {
-                System.out.print((char) current);
-                current = reader.read();
-            }
-        } finally {
-            reader.close();
+        reader = new FileReader("Tema 01/BasicJavaExercises/Ejercicio05/file.txt");
+        int current = reader.read();
+        while (current != -1) {
+            System.out.print((char) current);
+            current = reader.read();
         }
+        reader.close();
     }
 }
