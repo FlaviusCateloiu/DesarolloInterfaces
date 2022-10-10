1913dfcd8b13ed5b2e95c770eb4f179fed0ede59
flaars <fcateloiu08@gmail.com>
2022-10-10 20:31:56 +0200

    Faltan las demas del ejercicio 2 apartado 4

--- Tema 02/Ejercicio02VentanasComponentes/Ejercicio02_4/Ejercicio02_4.java
+++ Tema 02/Ejercicio02VentanasComponentes/Ejercicio02_4/Ejercicio02_4.java
@@ -1,16 +1,30 @@
 import javax.swing.*;
+import java.awt.*;
 import java.util.Scanner;
 
 public class Ejercicio02_4 {
 
-    private JLabel textExplainProgram;
+    private JLabel explainProgramJLabel;
+    private JLabel celsiusJLabel;
+    private JTextField celsiusJTextField;
+    private JLabel farenheitJLabel;
+    private JTextField farenheitJTextField;
 
     public Ejercicio02_4() {
         JFrame windowsExplain = new JFrame("Celsius to Farenheit");
-        textExplainProgram = new JLabel("Este programa es para calcular Celsius a Farenheit y viceversa.");
-        windowsExplain.add(textExplainProgram);
+        windowsExplain.setLayout(new FlowLayout(FlowLayout.CENTER, 20, 20));
+        explainProgramJLabel = new JLabel("Este programa es para calcular Celsius a Farenheit y viceversa.", SwingConstants.CENTER);
+        celsiusJLabel = new JLabel("Celsius:");
+        celsiusJTextField = new JTextField("0");
+        farenheitJLabel = new JLabel("Farenheit:");
+        farenheitJTextField = new JTextField("0");
+        windowsExplain.add(explainProgramJLabel);
+        windowsExplain.add(celsiusJLabel);
+        windowsExplain.add(celsiusJTextField);
+        windowsExplain.add(farenheitJLabel);
+        windowsExplain.add(farenheitJTextField);
         windowsExplain.setVisible(true);
-        windowsExplain.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        windowsExplain.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         windowsExplain.setLocationRelativeTo(null);
         windowsExplain.setSize(600, 300);
     }
