3643387ec66cb1aa50e3653b0a6685ced700fd76
flaars <fcateloiu08@gmail.com>
2022-10-14 17:30:40 +0200

    Modificación lo de alejandro

--- Tema 02/Ejercicio03FicherosMasVentanas/Ejercicio5.3/Ejercicio5_3.java
+++ Tema 02/Ejercicio03FicherosMasVentanas/Ejercicio5.3/Ejercicio5_3.java
@@ -1,80 +1,41 @@
+import javax.swing.*;
+import java.awt.*;
 import java.io.*;
 import java.nio.file.Path;
-import java.nio.file.StandardOpenOption;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 
 public class Ejercicio5_3 {
+
+    private JButton nuevaPersona;
+    private JButton mostrarTodos;
+    private JButton buscarPersona;
+
+    public Ejercicio5_3() {
+        JFrame ventanaPrincipal = new JFrame("Ventana Principal");
+        this.nuevaPersona = new JButton("Nueva Persona");
+        this.mostrarTodos = new JButton("Mostrar Todos");
+        this.buscarPersona = new JButton("Buscar Persona");
+        ventanaPrincipal.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 10));
+        ventanaPrincipal.add(this.nuevaPersona);
+        ventanaPrincipal.add(this.mostrarTodos);
+        ventanaPrincipal.add(this.buscarPersona);
+        ventanaPrincipal.setLocationRelativeTo(null);
+        ventanaPrincipal.setSize(500, 100);
+        ventanaPrincipal.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        ventanaPrincipal.setVisible(true);
+    }
     public static void main(String[] args) throws Exception {
-        BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));
         ArrayList<Persona> listaPersonas = new ArrayList<>();
-        boolean formatoFecha, personaRepetida;
-        String op = "", nombre = "", email = "", fechaNacimiento = "";
         Date fecha = null;
         Persona pers;
 
         cargarPersonas(listaPersonas);
-
-        do {
-            System.out.printf("%s MENU AGENDA %s\n", "-".repeat(10), "-".repeat(10));
-            System.out.println("1.- Introducir a una Persona.");
-            System.out.println("2.- Buscar a una Persona.");
-            System.out.println("3.- Salir del programa.");
-            System.out.print("Introduce el numero de la opcion que quieres: ");
-            op = sc.readLine();
-            System.out.println();
-
-            switch (op) {
-                case "1":
-                    System.out.print("Introduce el nombre: ");
-                    nombre = sc.readLine();
-
-                    System.out.print("Introudce el email: ");
-                    email = sc.readLine();
-                    do {
-                        try {
-                            System.out.print("Introduce la fecha de nacimiento con el formato dd/MM/yyyy: ");
-                            fechaNacimiento = sc.readLine();
-
-                            fecha = new SimpleDateFormat("dd/MM/yyyy").parse(fechaNacimiento);
-                            formatoFecha = false;
-                        } catch (Exception e) {
-                            formatoFecha = true;
-                            System.out.println("Formato de la fecha incorrecto.");
-                        }
-                    } while (formatoFecha);
-                    System.out.println();
-
-                    borrarPersona(listaPersonas, nombre);
-                    pers = new Persona(nombre, email, fecha);
-                    listaPersonas.add(pers);
-                    guardarPersonas(listaPersonas);
-                    break;
-                case "2":
-                    System.out.print("Introduce el nombre de la persona que buscar: ");
-                    nombre = sc.readLine();
-
-                    pers = buscarPersona(nombre, listaPersonas);
-
-                    if (pers == null) {
-                        System.out.println("Error, has introducido en nombre de una persona que no esta en la lista prueba a poner otro.");
-                    } else {
-                        System.out.println(pers);
-                    }
-                    System.out.println();
-                    break;
-                case "3":
-                    break;
-                default:
-                    System.out.println("Error has introducido una opcion del menu incorrecta.");
-            }
-
-        } while (!op.equals("3"));
+        Ejercicio5_3 ventanaPrincipal = new Ejercicio5_3();
     }
 
     public static void guardarPersonas(ArrayList<Persona> listaPersonas) {
-        Path rutaArchivo = Path.of("Unidad1/Ejercicio05Serializacion/Ejercicio5.1/personas.dat");
+        Path rutaArchivo = Path.of("Tema 02/Ejercicio03FicherosMasVentanas/Ejercicio5.3/personas.dat");
         try (FileOutputStream fos = new FileOutputStream(rutaArchivo.toFile());
              ObjectOutputStream oos = new ObjectOutputStream(fos)) {
             for (Persona pers : listaPersonas) {
@@ -86,7 +47,7 @@ public class Ejercicio5_3 {
     }
 
     public static void cargarPersonas(ArrayList<Persona> listaPersonas) {
-        Path rutaArchivo = Path.of("Unidad1/Ejercicio05Serializacion/Ejercicio5.1/personas.dat");
+        Path rutaArchivo = Path.of("Tema 02/Ejercicio03FicherosMasVentanas/Ejercicio5.3/personas.dat");
         Persona pers;
         try (FileInputStream fis = new FileInputStream(rutaArchivo.toFile());
              ObjectInputStream ois = new ObjectInputStream(fis)) {
