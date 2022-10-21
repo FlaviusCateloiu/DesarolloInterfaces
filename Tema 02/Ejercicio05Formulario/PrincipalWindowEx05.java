32e1ba590e7aa43c2622b640a8e742c7af2a9035
flaars <fcateloiu08@gmail.com>
2022-10-21 17:40:26 +0200

    Empezando el ejercicio.

---
+++ Tema 02/Ejercicio05Formulario/PrincipalWindowEx05.java
@@ -0,0 +1,45 @@
+import javax.swing.*;
+
+public class PrincipalWindowEx05 extends JFrame {
+
+    private JLabel JLFormulario;
+    private JLabel JLNombre;
+    private JTextField JTFNombre;
+    private JLabel TLNombreError;
+    private JLabel JLApellidos;
+    private JTextField JTFApellidos;
+    private JLabel JLApellidosError;
+    private JLabel JLDni;
+    private JTextField JTFDni;
+    private JLabel JLDniError;
+    private JLabel JLTelefono;
+    private JTextField JTFTelefono;
+    private JLabel JLTelefonoError;
+    private JLabel JLCalle;
+    private JTextField JTFCalle;
+    private JLabel JLCalleError;
+    private JLabel JLPortal;
+    private JTextField JTFPortal;
+    private JLabel JLPortalError;
+    private JLabel JLPiso;
+    private JTextField JTFPiso;
+    private JLabel JLPuerta;
+    private JTextField JTFPuerta;
+    private JLabel JLCodigoPostal;
+    private JTextField JTFCodigoPostal;
+    private JLabel JLCodigoPostalError;
+    private JLabel JLCiudad;
+    private JTextField JTFCiudad;
+    private JLabel JLCiudadError;
+    private JLabel JLNumFederador;
+    private JTextField JTFNumFederador;
+    private JLabel JLNumFederadorError;
+    private JLabel JLSexo;
+
+    public PrincipalWindowEx05() {
+        super("Formulario");
+    }
+    public static void main(String[] args) {
+
+    }
+}
