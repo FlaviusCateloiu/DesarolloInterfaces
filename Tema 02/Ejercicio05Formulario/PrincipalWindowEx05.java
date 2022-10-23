b174c0e1cfbf37179f010448e2d91ec3ca6a32aa
Flavius <fcateloiu08@gmail.com>
2022-10-23 13:31:49 +0200

    Sigo los componentes de la ventana.

--- Tema 02/Ejercicio05Formulario/PrincipalWindowEx05.java
+++ Tema 02/Ejercicio05Formulario/PrincipalWindowEx05.java
@@ -40,7 +40,7 @@ public class PrincipalWindowEx05 extends JFrame {
 
     public PrincipalWindowEx05() {
         super("Formulario");
-        JLNombre = new JLabel("Nombre:");
+        JLNombre = new JLabel("*Nombre:");
         JLNombre.setBounds(30, 30, 100, 20);
         add(JLNombre);
         JTFNombre = new JTextField("");
@@ -51,7 +51,7 @@ public class PrincipalWindowEx05 extends JFrame {
         JLNombreError.setBounds(30, 50, 340, 20);
         add(JLNombreError);
 
-        JLApellidos = new JLabel("Apellidos:");
+        JLApellidos = new JLabel("*Apellidos:");
         JLApellidos.setBounds(380, 30, 100, 20);
         add(JLApellidos);
         JTFApellidos = new JTextField("");
@@ -62,27 +62,78 @@ public class PrincipalWindowEx05 extends JFrame {
         JLApellidosError.setBounds(380, 50, 400, 20);
         add(JLApellidosError);
 
-        JLDni = new JLabel("DNI:");
+        JLDni = new JLabel("*DNI:");
         JLDni.setBounds(750, 30, 50, 20);
         add(JLDni);
         JTFDni = new JTextField("");
-        JTFDni.setBounds(785, 30, 100, 20);
+        JTFDni.setBounds(785, 30, 80, 20);
         add(JTFDni);
         JLDniError = new JLabel("Error el tienes que introducir el dni con formato correcto.");
         JLDniError.setForeground(Color.RED);
         JLDniError.setBounds(750, 50, 400, 20);
         add(JLDniError);
 
-        JLTelefono = new JLabel("Telefono:");
-        JLTelefono.setBounds(30, 90, 100, 20);
+        JLTelefono = new JLabel("*Telefono:");
+        JLTelefono.setBounds(30, 80, 100, 20);
         add(JLTelefono);
         JTFTelefono = new JTextField("");
-        JTFTelefono.setBounds(90, 90, 100, 20);
+        JTFTelefono.setBounds(90, 80, 80, 20);
         add(JTFTelefono);
+        JLTelefonoError = new JLabel("Error el telefono es maximo de 9 numeros.");
+        JLTelefonoError.setForeground(Color.RED);
+        JLTelefonoError.setBounds(30, 100, 250, 20);
+        add(JLTelefonoError);
+
+        JLCalle = new JLabel("*Calle:");
+        JLCalle.setBounds(300, 80, 100, 20);
+        add(JLCalle);
+        JTFCalle = new JTextField("");
+        JTFCalle.setBounds(340, 80, 200, 20);
+        add(JTFCalle);
+        JLCalleError = new JLabel("Error tienes que introducir la calle.");
+        JLCalleError.setForeground(Color.RED);
+        JLCalleError.setBounds(300, 100, 250, 20);
+        add(JLCalleError);
+
+        JLPortal = new JLabel("*Numero Portal:");
+        JLPortal.setBounds(570, 80, 100, 20);
+        add(JLPortal);
+        JTFPortal = new JTextField("");
+        JTFPortal.setBounds(665, 80, 35, 20);
+        add(JTFPortal);
+        JLPortalError = new JLabel("Error tienes que introducir el portal.");
+        JLPortalError.setForeground(Color.RED);
+        JLPortalError.setBounds(570, 100, 250, 20);
+        add(JLPortalError);
+
+        JLPiso = new JLabel("Numero Piso:");
+        JLPiso.setBounds(790, 80, 100, 20);
+        add(JLPiso);
+        JTFPiso = new JTextField("");
+        JTFPiso.setBounds(875, 80, 30, 20);
+        add(JTFPiso);
+
+        JLPuerta = new JLabel("Puerta:");
+        JLPuerta.setBounds(930, 80, 100, 20);
+        add(JLPuerta);
+        JTFPuerta = new JTextField("");
+        JTFPuerta.setBounds(980, 80, 30, 20);
+        add(JTFPuerta);
+
+        JLCodigoPostal = new JLabel("*Codigo Postal:");
+        JLCodigoPostal.setBounds(30, 130, 100, 20);
+        add(JLCodigoPostal);
+        JTFCodigoPostal = new JTextField("");
+        JTFCodigoPostal.setBounds(120, 130, 50, 20);
+        add(JTFCodigoPostal);
+        JLCodigoPostalError = new JLabel("Error tienes que introducir el portal.");
+        JLCodigoPostalError.setForeground(Color.RED);
+        JLCodigoPostalError.setBounds(30, 150, 250, 20);
+        add(JLCodigoPostalError);
 
         setLayout(null);
         setResizable(false);
-        setSize(1200, 800);
+        setSize(1120, 700);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setVisible(true);
