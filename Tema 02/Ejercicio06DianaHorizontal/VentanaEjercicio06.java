0dbe8a4ff018d4c0724c644ad35b2109a932577f
Flavius <fcateloiu08@gmail.com>
2022-11-01 10:06:12 +0100

    Terminado con exito.

--- Tema 02/Ejercicio06DianaHorizontal/VentanaEjercicio06.java
+++ Tema 02/Ejercicio06DianaHorizontal/VentanaEjercicio06.java
@@ -5,21 +5,42 @@ public class VentanaEjercicio06 extends JFrame  {
     private JSlider controler;
     private JButton botonVolverJugar;
 
+    private JButton botonNivel1;
+    private JButton botonNivel2;
+    private JButton botonNivel3;
+    private JButton botonAcercaDe;
+
     public VentanaEjercicio06() {
         super("Diana Horizontal");
         setLayout(null);
         setResizable(false);
 
         botonVolverJugar = new JButton("Volver a Jugar");
-        botonVolverJugar.setBounds(20, 120, 150, 30);
+        botonVolverJugar.setBounds(1720, 120, 150, 30);
         add(botonVolverJugar);
+
+        botonNivel1 = new JButton("Nivel1");
+        botonNivel1.setBounds(20, 120, 150, 30);
+        add(botonNivel1);
+
+        botonNivel2 = new JButton("Nivel2");
+        botonNivel2.setBounds(200, 120, 150, 30);
+        add(botonNivel2);
+
+        botonNivel3 = new JButton("Nivel3");
+        botonNivel3.setBounds(400, 120, 150, 30);
+        add(botonNivel3);
+
+        botonAcercaDe = new JButton("Acerca De");
+        botonAcercaDe.setBounds(1720, 10, 150, 30);
+        add(botonAcercaDe);
 
         controler = new JSlider();
         controler.setBounds(20, 60, 1920/2, 30);
         controler.setValue(0);
         add(controler);
 
-        setSize(1000, 200);
+        setSize(1920, 200);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         setVisible(true);
@@ -32,9 +53,23 @@ public class VentanaEjercicio06 extends JFrame  {
             }
         });
 
+        botonNivel1.addActionListener(e -> {
+            controler.setBounds(20, 60, 1920/10, 30);
+        });
+
+        botonNivel2.addActionListener(e -> {
+            controler.setBounds(20, 60, 1920/2, 30);
+        });
+
+        botonNivel3.addActionListener(e -> {
+            controler.setBounds(20, 60, 1860, 30);
+        });
+        botonAcercaDe.addActionListener(e -> {
+            VentanaScoreEj06 ventanaAcercaDe = new VentanaScoreEj06(this);
+        });
+
         botonVolverJugar.addActionListener(e -> {
             valorRamdom.set((int) (Math.random() * 100));
-            controler.setValue(0);
         });
     }
 
--- Tema 02/Ejercicio06DianaHorizontal/VentanaScoreEj06.java
+++ Tema 02/Ejercicio06DianaHorizontal/VentanaScoreEj06.java
@@ -3,6 +3,32 @@ import java.awt.*;
 
 public class VentanaScoreEj06 extends JDialog {
     private JLabel score;
+    private JButton volver;
+
+    public VentanaScoreEj06(JFrame jFrame) {
+        super(jFrame, "Acerca De");
+        setLayout(null);
+
+        score = new JLabel();
+        score.setBounds(10, 10, 2000, 20);
+        score.setText("Contiene cuatro JButton Tres para los diferentes niveles y uno para volver a jugar, un JSlider " +
+                "que es valor para ver si aciertas en la clase principal y despues en la clase del JDialog contiene un " +
+                "JButton para volver y un JLabel mostrar el contenido.");
+
+        volver = new JButton("Volver");
+        volver.setBounds(1350, 100, 80, 30);
+        add(volver);
+        add(score);
+
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        setLocationRelativeTo(jFrame);
+        setVisible(true);
+        setSize(1500, 200);
+
+        volver.addActionListener(e -> {
+            this.dispose();
+        });
+    }
 
     public VentanaScoreEj06(JFrame jFrame, int valor, int valorRandom) {
         super(jFrame, "Resultado de la diana");
@@ -14,25 +40,32 @@ public class VentanaScoreEj06 extends JDialog {
             score.setText("¡Enhorabuena, acertaste! " + valor);
             score.setForeground(Color.RED);
         } else {
-            if (valorRandom <= 10 || valorRandom >= 90) {
-                if (valor < valorRandom || valor > valorRandom) {
-                    score.setText("Ha estado cerca, prueba otra vez " + valor);
-                    score.setForeground(new Color(0xFD7300));
-                }
-
-            } else {
+            if (valor >= (valorRandom - 10) && valor < valorRandom) {
                 score.setText("Ha estado cerca, prueba otra vez " + valor);
                 score.setForeground(new Color(0xFD7300));
+            } else {
+                if (valor <= (valorRandom + 10) && valor > valorRandom) {
+                    score.setText("Ha estado cerca, prueba otra vez " + valor);
+                    score.setForeground(new Color(0xFD7300));
+                } else {
+                    score.setText("Ha estado lejos, prueba otra vez " + valor);
+                    score.setForeground(new Color(0x459635));
+                }
             }
-            score.setText("Ha estado lejos, prueba otra vez " + valor);
-            score.setForeground(new Color(0x459635));
         }
 
+        volver = new JButton("Volver");
+        volver.setBounds(150, 100, 80, 30);
+        add(volver);
         add(score);
 
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         setLocationRelativeTo(jFrame);
         setVisible(true);
-        setSize(300, 100);
+        setSize(300, 200);
+
+        volver.addActionListener(e -> {
+            this.dispose();
+        });
     }
 }
