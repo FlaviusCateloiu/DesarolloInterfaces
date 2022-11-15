edc89e8cd4c621ccb56ce89d6b58bda72d424311
flaars <fcateloiu08@gmail.com>
2022-11-15 17:19:31 +0100

    Aprender a utilizar la GUI de Intellig

--- Tema 03/BattleShip/BattleShipMain.java
+++ Tema 03/BattleShip/BattleShipMain.java
@@ -1,7 +1,5 @@
 public class BattleShipMain {
     public static void main(String[] args) {
-        Tablero t = new Tablero();
-        t.generarTablero();
-        System.out.println(t);
+
     }
 }
---
+++ Tema 03/BattleShip/PantallaPrincipal.java
@@ -0,0 +1,5 @@
+import javax.swing.*;
+
+public class PantallaPrincipal {
+    private JButton button1;
+}
