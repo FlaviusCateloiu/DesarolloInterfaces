7a05b081d8dc49a7076a88e56019cdcda92cde78
Flavius <fcateloiu08@gmail.com>
2022-11-20 17:23:32 +0100

    No tengo ni idea de como hacer el mapa para que clique el usuario y se cambie el color porque ha sido clicado.

--- Tema 03/BattleShip/Mapa.java
+++ Tema 03/BattleShip/Mapa.java
@@ -1,13 +1,11 @@
 import javax.swing.*;
 import javax.swing.border.LineBorder;
 import java.awt.*;
-import java.util.List;
 
 public class Mapa extends JPanel {
     public Mapa() {
         super();
         setLayout(new GridLayout(11, 11));
-        setSize(300, 300);
         for (int i = 0; i < 121; i++) {
             JLabel label = new JLabel("");
             label.setBorder(new LineBorder(Color.BLACK));
