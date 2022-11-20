495895712e3874d0d0bb76e64ede3ba2645f122c
Flavius <fcateloiu08@gmail.com>
2022-11-20 17:30:47 +0100

    Algo tengo por ahora.

--- Tema 03/BattleShip/Mapa.java
+++ Tema 03/BattleShip/Mapa.java
@@ -1,8 +1,12 @@
 import javax.swing.*;
 import javax.swing.border.LineBorder;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.List;
 
 public class Mapa extends JPanel {
+
     public Mapa() {
         super();
         setLayout(new GridLayout(11, 11));
@@ -10,6 +14,16 @@ public class Mapa extends JPanel {
             JLabel label = new JLabel("");
             label.setBorder(new LineBorder(Color.BLACK));
             add(label);
+            label.addMouseListener(new MouseAdapter() {
+                @Override
+                public void mouseClicked(MouseEvent e) {
+                    label.setText("Marcado");
+                }
+            });
         }
+    }
+
+    public List<Component> getComponentes() {
+        return this.getComponentes();
     }
 }
