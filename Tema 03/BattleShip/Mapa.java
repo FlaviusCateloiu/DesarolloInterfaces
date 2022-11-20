58ab4e62ba6e36289b297d379e61326a6dac7405
Flavius <fcateloiu08@gmail.com>
2022-11-20 15:42:33 +0100

    Faltan muchas cosas

--- Tema 03/BattleShip/Mapa.java
+++ Tema 03/BattleShip/Mapa.java
@@ -1,15 +1,9 @@
-import javax.sound.sampled.Line;
 import javax.swing.*;
 import javax.swing.border.LineBorder;
 import java.awt.*;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.util.ArrayList;
 import java.util.List;
 
 public class Mapa extends JPanel {
-
-    private List<JLabel> listaLabels = new ArrayList<>();
     public Mapa() {
         super();
         setLayout(new GridLayout(11, 11));
@@ -17,16 +11,7 @@ public class Mapa extends JPanel {
         for (int i = 0; i < 121; i++) {
             JLabel label = new JLabel("");
             label.setBorder(new LineBorder(Color.BLACK));
-            listaLabels.add(label);
             add(label);
         }
-    }
-
-    public List<JLabel> getListaLabels() {
-        return listaLabels;
-    }
-
-    public void setListaLabels(List<JLabel> listaLabels) {
-        this.listaLabels = listaLabels;
     }
 }
