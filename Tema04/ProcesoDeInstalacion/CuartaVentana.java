4c2e586c79bb58a4eb65df8f406b5541d5232fd7
flaars <fcateloiu08@gmail.com>
2022-12-16 19:09:00 +0100

    Terminado

--- Tema04/ProcesoDeInstalacion/CuartaVentana.java
+++ Tema04/ProcesoDeInstalacion/CuartaVentana.java
@@ -1,9 +1,26 @@
 package ProcesoDeInstalacion;
 
 import javax.swing.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
-public class CuartaVentana {
+public class CuartaVentana extends JFrame {
     private JButton finalizarButton;
     private JTextArea laInstalacionSeHaTextArea;
     private JPanel jPCuartaVentana;
+    private JCheckBox ejecutarProgramaAlFinalizarCheckBox;
+
+    public CuartaVentana() {
+        super("Cuarta Ventana");
+
+        finalizarButton.addActionListener(a -> {
+            System.exit(0);
+        });
+
+        setContentPane(jPCuartaVentana);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setSize(800, 200);
+        setVisible(true);
+    }
 }
--- Tema04/ProcesoDeInstalacion/PrimeraVentana.java
+++ Tema04/ProcesoDeInstalacion/PrimeraVentana.java
@@ -1,15 +1,48 @@
 package ProcesoDeInstalacion;
 
 import javax.swing.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
-public class PrimeraVentana {
+public class PrimeraVentana extends JFrame {
     private JPanel jPPrimeraVentana;
     private JButton aceptarButton;
     private JCheckBox jCBoxAcepCond;
-    private JTextArea jTACondiciones;
     private JScrollPane jSPCondiones;
+    private JTextPane aspectosLegalesDeLasTextPane;
 
-    private void createUIComponents() {
-        // TODO: place custom component creation code here
+    public PrimeraVentana() {
+        super("Primera Ventana");
+
+        jCBoxAcepCond.addActionListener(a -> {
+            if (jCBoxAcepCond.isSelected()) {
+                aceptarButton.setEnabled(true);
+            } else {
+                aceptarButton.setEnabled(false);
+            }
+        });
+
+        aceptarButton.addActionListener(a -> {
+            setVisible(false);
+            SegundaVentana segundaVentana = new SegundaVentana(this);
+        });
+
+        this.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosed(WindowEvent e) {
+                super.windowClosed(e);
+                System.exit(0);
+            }
+        });
+        
+        setContentPane(jPPrimeraVentana);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setSize(800, 500);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        PrimeraVentana ventana = new PrimeraVentana();
     }
 }
