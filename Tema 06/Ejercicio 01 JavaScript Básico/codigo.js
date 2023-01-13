b6927e24dfcf1f0116ab029d2225af87f499402e
flaars <fcateloiu08@gmail.com>
2023-01-13 16:58:07 +0100

    Terminado el ejercicio sencillo de JavaScript

---
+++ Tema 06/Ejercicio 01 JavaScript Básico/codigo.js
@@ -0,0 +1,6 @@
+let mensaje = "Hola Mundo! Que facil es incluir 'comillas simples' y " + '"comillas dobles"';
+// Muestras por consola el texto que contiene dentro.
+window.alert(mensaje);
+console.log(mensaje);
+// Sirve para cambiar o introducir contenido en la etiqueta que contiene el id "demo".
+document.getElementById("demo").innerHTML = "Soy el primer script";
