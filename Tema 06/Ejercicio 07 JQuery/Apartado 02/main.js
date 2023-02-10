439c22e85ca6ac4f18a82b3fb14d9a862816308c
flaars <fcateloiu08@gmail.com>
2023-02-10 17:07:10 +0100

    Apartado 02 ya esta

---
+++ Tema 06/Ejercicio 07 JQuery/Apartado 02/main.js
@@ -0,0 +1,13 @@
+$("document").ready(function () {
+    let imagenes = $("div img");
+    imagenes.click(function () {
+       imagenes.css("border-width", "0");
+       imagenes.css("opacity", 0.4);
+       $(this).css("border", "solid 5px black");
+       $(this).css("opacity", 1);
+    });
+    imagenes.mouseleave(function () {
+        imagenes.css("border-width", "0");
+        imagenes.css("opacity", 1);
+    });
+});
