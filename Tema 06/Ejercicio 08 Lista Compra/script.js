94717f0b7cc7f204f9756eadaa86315efbaaa469
flaars <fcateloiu08@gmail.com>
2023-02-14 18:58:18 +0100

    Me falta el reloj.

--- Tema 06/Ejercicio 08 Lista Compra/script.js
+++ Tema 06/Ejercicio 08 Lista Compra/script.js
@@ -1,10 +1,22 @@
 $(document).ready(function () {
-    $("input[type=button]").dblclick(function () {
-        window.alert("adios")
+    $("#anadir-lista").click(function () {
+        let texto = $("#anadir").val();
+        if (texto.match(/^.+$/)) {
+            texto = texto + " - " + new Date().toLocaleString();
+            $("#listaCompra").append("<li>" + texto + "</li>");
+            window.localStorage.setItem("product" + window.localStorage.length, texto);
+        } else {
+            console.log("Error tienes que introducir algo en el texto.");
+        }
     });
 
-    $("input[type=button]").click(function () {
-       let texto = $("anadir").getText();
-       window.alert(texto);
+    $("#anadir-lista").dblclick(function () {
+        $("#listaCompra li").remove();
+    });
+
+    $("#resetear-lista").click(function () {
+        for (let i = 0; i < window.localStorage.length; i++) {
+            $("#listaCompra").append("<li>" + window.localStorage.getItem("product" + i) + "</li>");
+        }
     });
 });
