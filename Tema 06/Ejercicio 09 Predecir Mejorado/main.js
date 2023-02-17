97d8e0644a1c78a58843a235edf1f48561cb9ba9
flaars <fcateloiu08@gmail.com>
2023-02-17 19:04:35 +0100

    Me falta la parte de las frases que salgan todas.

--- Tema 06/Ejercicio 09 Predecir Mejorado/main.js
+++ Tema 06/Ejercicio 09 Predecir Mejorado/main.js
@@ -1,35 +1,60 @@
 $(document).ready(function () {
-    let maxRandom = 10;
+    let maxRandom = 0
+    let arrayFrasesUtilizar = [false, false, false, false, false, false, false, false, false, false];
     let arrayFrases = ["Te esperan novedades en el dinero.", "Tu vida sentimental va a dar un vuelco.", "¿Has pensado en comprar lotería? ¡La suerte está contigo!", "Si no puedes convencerlos, confundelos.", "Aparecera en tu vida  una persona muy especial.", "El dia de hoy la gente te prestara mas atencion.", "Tendras un desafortunado encuentro.", "Tu amor a la música será una parte importante de tu vida.", "Un cambio importante se esta gestando, deberas ser observador.", "La suerte puede estar en una galleta."];
+
+    for (let i = 0; i < arrayFrases.length; i++) {
+        window.localStorage.setItem(("frase" + i), arrayFrases[i]);
+    }
+
+    if (window.localStorage.getItem("nombre") !== null) {
+        maxRandom = window.localStorage.length - 1;
+    } else {
+        maxRandom = window.localStorage.length;
+    }
+
     $("#boton").click(async function () {
         let nombre = $("#nombre").val();
-        let numeroAleatorio = Math.floor(Math.random() * maxRandom);
-        $("#resultado").text("Consultando a los espíritus…");
-        await new Promise(r => setTimeout(r, 2000));
-        if (nombre.match(/^[A-Za-zÀ-ÿ]+$/)) {
-            window.localStorage.setItem("nombre", nombre);
-            $("#nombre").val("");
-            $("#resultado").text(window.localStorage.getItem("nombre") + ", " + arrayFrases[numeroAleatorio]);
-        } else {
-            //Comprobar que la cookie contiene algun nombre para saber si sacar el resultado con ese nombre o no.
-            if (window.localStorage.getItem("nombre") !== null) {
-                $("#resultado").text(window.localStorage.getItem("nombre") + ", " + arrayFrases[numeroAleatorio]);
-            } else {
-                $("#resultado").text(arrayFrases[numeroAleatorio]);
+        let adios = false;
+        while (!adios) {
+            let numeroAleatorio = Math.floor(Math.random() * maxRandom);
+            adios = arrayFrasesUtilizar[numeroAleatorio];
+            if (!adios) {
+                $("#resultado").text("Consultando a los espíritus…");
+                await new Promise(r => setTimeout(r, 2000));
+                if (nombre.match(/^[A-Za-zÀ-ÿ]+$/)) {
+                    window.localStorage.setItem("nombre", nombre);
+                    $("#nombre").val("");
+                    $("#resultado").text(window.localStorage.getItem("nombre") + ", " + window.localStorage.getItem("frase" + numeroAleatorio));
+                    arrayFrasesUtilizar[numeroAleatorio] = true;
+                    adios = true;
+                } else {
+                    //Comprobar que la cookie contiene algun nombre para saber si sacar el resultado con ese nombre o no.
+                    if (window.localStorage.getItem("nombre") !== null) {
+                        $("#resultado").text(window.localStorage.getItem("nombre") + ", " + window.localStorage.getItem("frase" + numeroAleatorio));
+                    } else {
+                        $("#resultado").text(window.localStorage.getItem("frase" + numeroAleatorio));
+                    }
+                    arrayFrasesUtilizar[numeroAleatorio] = true;
+                    adios = true;
+                }
             }
         }
+
+        let checker = arr => arr.every(Boolean);
+        console.log(checker(arrayFrasesUtilizar));
     });
 
     $("#anyadir").click(function () {
         let fraseAnyadir = $("#frase").val();
         if (fraseAnyadir !== "") {
-            arrayFrases.push(fraseAnyadir);
+            window.localStorage.setItem(("frase" + maxRandom), fraseAnyadir);
+            arrayFrasesUtilizar.push(false);
             maxRandom++;
-            window.alert(fraseAnyadir + " se ha añadido con exito.")
             $("#frase").val("");
         } else {
-            window.alert("Tienes que introducir una frase.")
+            window.alert("Error tienes que introducir una frase.");
         }
     });
     document.getElementById("sound").play();
-})
+});
