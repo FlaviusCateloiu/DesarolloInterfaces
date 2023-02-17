7d183f545248884af2f7124005381889d4593363
flaars <fcateloiu08@gmail.com>
2023-02-17 15:58:25 +0100

    Terminada con exito.

--- Tema 06/Ejercicio 08 Lista Compra/script.js
+++ Tema 06/Ejercicio 08 Lista Compra/script.js
@@ -4,19 +4,21 @@ $(document).ready(function () {
         if (texto.match(/^.+$/)) {
             texto = texto + " - " + new Date().toLocaleString();
             $("#listaCompra").append("<li>" + texto + "</li>");
+            $("#listaCompra li").dblclick(function () {
+                this.remove();
+            });
             window.localStorage.setItem("product" + window.localStorage.length, texto);
         } else {
             console.log("Error tienes que introducir algo en el texto.");
         }
     });
 
-    $("#anadir-lista").dblclick(function () {
-        $("#listaCompra li").remove();
-    });
-
     $("#resetear-lista").click(function () {
         for (let i = 0; i < window.localStorage.length; i++) {
             $("#listaCompra").append("<li>" + window.localStorage.getItem("product" + i) + "</li>");
+            $("#listaCompra li").dblclick(function () {
+                this.remove();
+            });
         }
     });
 });
---
+++ Tema 06/Ejercicio 09 Predecir Mejorado/main.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+    let maxRandom = 10;
+    let arrayFrases = ["Te esperan novedades en el dinero.", "Tu vida sentimental va a dar un vuelco.", "¿Has pensado en comprar lotería? ¡La suerte está contigo!", "Si no puedes convencerlos, confundelos.", "Aparecera en tu vida  una persona muy especial.", "El dia de hoy la gente te prestara mas atencion.", "Tendras un desafortunado encuentro.", "Tu amor a la música será una parte importante de tu vida.", "Un cambio importante se esta gestando, deberas ser observador.", "La suerte puede estar en una galleta."];
+    $("#boton").click(async function () {
+        let nombre = $("#nombre").val();
+        let numeroAleatorio = Math.floor(Math.random() * maxRandom);
+        $("#resultado").text("Consultando a los espíritus…");
+        await new Promise(r => setTimeout(r, 2000));
+        if (nombre.match(/^[A-Za-zÀ-ÿ]+$/)) {
+            $("#resultado").text(nombre + ", " + arrayFrases[numeroAleatorio]);
+        } else {
+            $("#resultado").text(arrayFrases[numeroAleatorio]);
+        }
+    });
+
+    $("#anyadir").click(function () {
+        let fraseAnyadir = $("#frase").val();
+        if (fraseAnyadir !== "") {
+            arrayFrases.push(fraseAnyadir);
+            maxRandom++;
+            window.alert(fraseAnyadir + " se ha añadido con exito.")
+            $("#frase").val("");
+        } else {
+            window.alert("Tienes que introducir una frase.")
+        }
+    });
+    document.getElementById("sound").play();
+})
